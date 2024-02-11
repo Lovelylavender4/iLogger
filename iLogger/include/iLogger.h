ac8e76eb9e5fbd6d4db34400b996765a8232b4c0
Lovelylavender4 <MiracleForest@Outlook.com>
2024-02-11 18:47:36 +0800

    feat: init.

---
+++ iLogger/include/iLogger.h
@@ -0,0 +1,1 @@
+#include <cxxstd.h>
---
+++ iLogger/tests/iLoggerTest.cpp
@@ -0,0 +1,10 @@
+#include <iLogger.h>
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "cxxstd: " << __CURRENT_CXXSTD_VERSION__ << "\n";
+
+    return 0;
+}
