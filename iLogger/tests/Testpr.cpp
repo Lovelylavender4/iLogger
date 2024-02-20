6cf53ba618937aa60d4880acd929b522cb667d37
Lovelylavender4 <94376005+Lovelylavender4@users.noreply.github.com>
2024-02-20 13:39:27 +0800

    Update Testpr.cpp

--- iLogger/tests/Testpr.cpp
+++ iLogger/tests/Testpr.cpp
@@ -1,14 +1,14 @@
 /*#include <iostream>
- 123 -1-2
+ 123 -1-2-3
 int add（int a,int b=0）
 {
     return a+b;
 }
-456-2-3
+456-2-3-4
 int main（）
 {
     add（1+2）;
-786-3-4
+786-3-4-5
     return 0;
 }*/
 
